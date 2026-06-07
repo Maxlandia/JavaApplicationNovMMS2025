25f7f83724f238653fbe57721776acd469310246
Maxlandia <ephraimbelema@gmail.com>
2026-06-07 10:24:22 -0400

    Assignment 1

---
+++ DataType.java
@@ -0,0 +1,29 @@
+public class DataType{
+	public static void main(String[] args){
+		byte age = 25;
+		System.out.printf("You are %d years old%n", age);
+		
+		short quantity = 30000;
+		System.out.printf("I bought %,d quantity of Phones%n", quantity);
+		
+		int population = 2057315598;
+		System.out.printf("The population of the city states is %,d%n", population); 
+		
+		long worldPopulation = 1004943920480493L;
+		System.out.printf("The World population has jumped to %,d in just ten years%n",worldPopulation);
+		
+		float myBalance = 670394960.9588F;
+		System.out.printf("Your account balance is $%,.2f%n",myBalance);
+
+		double cbnBalance = 796949569786545394596976594.568767554;
+		System.out.printf("The CBN has $%,.2f%n", cbnBalance);
+
+
+		char symbol = '@';
+		System.out.printf("Your email address is asciicod571%cgmail.com%n", symbol);
+		
+		
+		boolean isJava = true;
+		System.out.printf("Do you love learning? %b%n", isJava);
+	}
+}
---
+++ assignment.java
@@ -0,0 +1,2 @@
+
+create a folder called java keywords open microsoft words, find the java keywords, and write short notes on each of them keywords save it and push to github
